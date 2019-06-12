7c4d9caab92c295432e8549c6688318e53986c69
Magali <magalilescanocorrea@gmail.com>
2019-06-12 19:36:44 +0200

    Parte final

--- Entregable_Magali/app/src/main/java/com/example/entregable_magali/MainActivity.java
+++ Entregable_Magali/app/src/main/java/com/example/entregable_magali/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.entregable_magali;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etName;
@@ -16,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         etName = findViewById(R.id.name);
-         etSurname = findViewById(R.id.surname);
-         etPhone = findViewById(R.id.phone);
-         etWeb= findViewById(R.id.web);
+        etName = findViewById(R.id.name);
+        etSurname = findViewById(R.id.surname);
+        etPhone = findViewById(R.id.phone);
+        etWeb = findViewById(R.id.web);
     }
 
-    public void showButtonPressed(View view){
+    public void showButtonPressed(View view) {
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
         String name = etName.getText().toString();
         String surname = etSurname.getText().toString();
@@ -31,8 +34,69 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("name", name);
         intent.putExtra("surname", surname);
         intent.putExtra("web", web);
-        intent.putExtra("phone",phone);
+        intent.putExtra("phone", phone);
         startActivity(intent);
     }
+
+
+    public void openDialog(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle(getString(R.string.app_name));
+
+        alertDialogBuilder.setMessage(getString(R.string.deleteAllFields)).setCancelable(false)
+                .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int d) {
+
+                        etName.setText("");
+                        etSurname.setText("");
+                        etPhone.setText("");
+                        etWeb.setText("");
+                    }
+                })
+
+                .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+
+    }
+
+
+    public void sendButtonPressed(View view) {
+
+
+        if (etName.getText().toString().trim().equalsIgnoreCase("")) {
+            etName.setError(getString(R.string.empty));
+        } else if (etSurname.getText().toString().trim().equalsIgnoreCase("")) {
+            etSurname.setError(getString(R.string.empty));
+        } else if (etPhone.getText().toString().trim().equalsIgnoreCase("")) {
+            etPhone.setError(getString(R.string.empty));
+        } else if (etWeb.getText().toString().trim().equalsIgnoreCase("")) {
+            etWeb.setError(getString(R.string.empty));
+
+        } else {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            String name = etName.getText().toString();
+            String surname = etSurname.getText().toString();
+            String web = etWeb.getText().toString();
+            int phone = Integer.parseInt(etPhone.getText().toString());
+            intent.putExtra("name", name);
+            intent.putExtra("surname", surname);
+            intent.putExtra("web", web);
+            intent.putExtra("phone", phone);
+            startActivity(intent);
+        }
+
+    }
+
 
 }
--- Entregable_Magali/app/src/main/java/com/example/entregable_magali/SecondActivity.java
+++ Entregable_Magali/app/src/main/java/com/example/entregable_magali/SecondActivity.java
@@ -1,7 +1,12 @@
 package com.example.entregable_magali;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -9,19 +14,55 @@ public class SecondActivity extends AppCompatActivity {
     TextView tvSurname;
     TextView tvWeb;
     TextView tvPhone;
+    TextView tvCounter;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tvName = findViewById(R.id.name);
+        tvSurname = findViewById(R.id.surname);
+        tvWeb = findViewById(R.id.web);
+        tvPhone = findViewById(R.id.phone);
+        tvCounter=findViewById(R.id.counter);
+
+
         String name = getIntent().getStringExtra("name");
         String surname = getIntent().getStringExtra("surname");
         String web = getIntent().getStringExtra("web");
-        int phone = getIntent().getIntExtra("phone",0);
+        int phone = getIntent().getIntExtra("phone", 0);
 
         tvName.setText(name);
         tvSurname.setText(surname);
         tvWeb.setText(web);
-        tvPhone.setText(phone);
+        tvPhone.setText(String.valueOf(phone));
+
+       prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        int counter = prefs.getInt("counter",0);
+        counter++;
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("counter", counter);
+        editor.commit();
+        tvCounter.setText(String.valueOf(prefs.getInt("counter",0)));
+
+    }
+
+
+    public void clickOnWeb(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String web = tvWeb.getText().toString();
+        intent.setData(Uri.parse(web));
+        startActivity(intent);
+    }
+
+    public void clickOnPhone(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        String phone = tvPhone.getText().toString();
+        intent.setData(Uri.parse("tel:" + phone));
+
+        startActivity(intent);
     }
 }
+
+
